5994192f0d8a17b23d4ab8456738f51628139064
Kim Chae Eun <kce3615@naver.com>
2021-07-21 12:13:45 +0900

    2021-07-21 React component 관련

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types"
 
-const foodILike = [
-  {
-    id: 1, 
-    name: "kimchi",
-    image: "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi-150x150.jpg",
-    rating: 5
-  },
-  {
-    id: 2, 
-    name: "bab",
-    image: "http://www.bibigo.com/img/kr/img_bap1.jpg",
-    rating: 4.3
-  },
-]
-
-function Food({name, picture, rating}){
-  return (
-  <div>
-    <h1>I like {name}</h1>
-    <h2>{rating}/5.0</h2>
-    <img src={picture} alt={name}/>
-  </div>
-  );
-}
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
-
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish => (
-      <Food 
-      key={dish.id} 
-      name={dish.name}
-      picture={dish.image} 
-      rating={dish.rating} />
-    ))}
-  </div>
-  );
+class App extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   console.log("hello");
+  // }
+  state = {
+    count: 0
+  }
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  // componentWillUnmount(){
+  //   console.log("Goodbye, cruel world");
+  // }
+  render(){
+    console.log("i am rendering");
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
